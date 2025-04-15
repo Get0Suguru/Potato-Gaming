8a12ef866905700f27b01725ac5bed8a6a5f20e3
Gautam Taneja <gautam45taneja@gmail.com>
2025-04-15 21:45:12 +0530

    Added main application class - basic Spring Boot structure ready

---
+++ Potato-Gaming/src/main/java/com/potato/gaming/Potato_Gaming/PotatoGamingApplication.java
@@ -0,0 +1,13 @@
+package com.potato.gaming.Potato_Gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoGamingApplication.class, args);
+	}
+
+}
